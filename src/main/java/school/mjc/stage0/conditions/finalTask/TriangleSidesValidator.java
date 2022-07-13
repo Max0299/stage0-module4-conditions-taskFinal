7a9a66ad4fc386e8a9423c40ca23adfc37bd985a
Max Ageev <ageev.makson@yandex.ru>
2022-07-13 17:04:45 +0300

    commit

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -1,7 +1,13 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
-    public void validate(double firstSide, double secondSide, double thirdSide) {
+    public void validate(double a, double b, double c) {
+
+        if(a > 0 && b > 0 && c > 0 && (a+b) > c && (a+c) > b && (c+b) > a){
+            System.out.println("this is a valid triangle");
+        }else {
+            System.out.println("it's not a triangle");
+        }
 
     }
 }
